dee8232703733f6fd99659d1af96f3f02d7814a7
Girma <93812274+206724@users.noreply.github.com>
2024-07-27 00:12:12 -0700

    commit

--- src/Pages/PlacesPage.jsx
+++ src/Pages/PlacesPage.jsx
@@ -17,22 +17,9 @@ export default function PlacesPage() {
         List of all Added Places
         <br />
         <Link
-          className=" inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
-          to={"/account/places/new"}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
+          className=" inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full" to={"/account/places/new"} >
+          <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
           </svg>
           Add new place
         </Link>
